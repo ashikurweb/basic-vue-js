0e715c211294406b70361adc90f9fe0bb31db1da
ashikurweb <ashikurrahman7194@gmail.com>
2025-03-23 14:29:06 +0600

    Components Props

---
+++ episode-6/js/components/AppButton.js
@@ -0,0 +1,28 @@
+export default {
+  template: `
+    <button 
+      :class="{
+        'text-white py-2 px-5 font-semibold rounded-lg transition-all disabled:cursor-not-allowed': true,
+        'bg-blue-500 hover:bg-blue-600': type === 'primary',
+        'bg-purple-500 hover:bg-purple-600': type === 'secondary',
+        'bg-green-500 hover:bg-green-600': type === 'success',
+        'is-loading': processing
+      }" 
+      :disabled="processing"
+    >
+      <slot />
+    </button>
+  `,
+
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+    },
+    
+    processing: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
